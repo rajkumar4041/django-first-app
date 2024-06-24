1477a5d66e77da152b5cee7eaf74f8acb6276a0a
Rajkumar Rathod <rajkumarrathod414@gmail.com>
2024-06-25 00:52:10 +0530

    Merge with unrelated histories

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+"""
+Merge with unrelated histories
+"""
 urlpatterns=[
     path('',views.home,name="home"),
     path('room/',views.room,name="room")
