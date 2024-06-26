8e72cfa56db8a8b620ac5b3227ff60d40578863a
Rajkumar Rathod <rajkumarrathod414@gmail.com>
2024-06-27 00:31:42 +0530

    feat: dynamic data routing

--- base/urls.py
+++ base/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 from . import views
 
 
-"""
-Merge with unrelated histories
-"""
 urlpatterns=[
     path('',views.home,name="home"),
-    path('room/',views.room,name="room")]
+    path('room/<str:pk>/',views.room,name="room")]
 
