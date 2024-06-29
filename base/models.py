d834e52480a46de02b0df046d27cda292d584b2e
Rajkumar Rathod <rajkumarrathod414@gmail.com>
2024-06-29 18:55:49 +0530

    add topic and message modal

--- base/models.py
+++ base/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Room(models.Model):
     # below some commented var are for later used
-    # host =
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=80)
 
     # null = true is for db that this is an optional field
@@ -19,5 +27,17 @@ class Room(models.Model):
     # auto now add is give the only one time date time when we save the form first time
     created = models.DateTimeField(auto_now_add=True)
 
+    # necessary to create a string version
+    # it just what we wanna return
     def __str__(self) -> str:
         return self.name
+
+
+class Message(models.Model):
+    # user =
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    updated_at = models.DateTimeField(auto_now=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.body[0:50]
